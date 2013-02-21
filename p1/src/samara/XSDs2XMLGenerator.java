b2bafcc970d09f2789160e3b0e0d2c1479fb4f4f
vasiliy.komkov <komkof@gmail.com>
2013-02-21 15:43:39 +0400

    reformat some pice of code

--- p1/src/samara/XSDs2XMLGenerator.java
+++ p1/src/samara/XSDs2XMLGenerator.java
@@ -28,51 +28,51 @@ public class XSDs2XMLGenerator {
      */
     @SuppressWarnings("unchecked")
     private static String generate(File[] schemaFiles, String rootName) {
-	// Process Schema files
-	List sdocs = new ArrayList();
-	for (File schemaFile : schemaFiles) {
-	    try {
-		sdocs.add(XmlObject.Factory.parse(schemaFile, (new XmlOptions()).setLoadLineNumbers()
-			.setLoadMessageDigest()));
-	    } catch (Exception e) {
-		System.out.println("Can not load schema file: " + schemaFile + ": ");
-		return null;
-	    }
-	}
+		// Process Schema files
+		List sdocs = new ArrayList();
+		for (File schemaFile : schemaFiles) {
+		    try {
+			sdocs.add(XmlObject.Factory.parse(schemaFile, (new XmlOptions()).setLoadLineNumbers()
+				.setLoadMessageDigest()));
+		    } catch (Exception e) {
+			System.out.println("Can not load schema file: " + schemaFile + ": ");
+			return null;
+		    }
+		}1
 
-	XmlObject[] schemas = (XmlObject[]) sdocs.toArray(new XmlObject[sdocs.size()]);
+		XmlObject[] schemas = (XmlObject[]) sdocs.toArray(new XmlObject[sdocs.size()]);
 
-	SchemaTypeSystem sts = null;
-	if (schemas.length > 0) {
-	    XmlOptions compileOptions = new XmlOptions();
-	    try {
-		sts = XmlBeans.compileXsd(schemas, XmlBeans.getBuiltinTypeSystem(), compileOptions);
-	    } catch (Exception e) {
-		System.out.println("Compilation error!");
-		return null;
-	    }
-	}
+		SchemaTypeSystem sts = null;
+		if (schemas.length > 0) {
+		    XmlOptions compileOptions = new XmlOptions();
+		    try {
+			sts = XmlBeans.compileXsd(schemas, XmlBeans.getBuiltinTypeSystem(), compileOptions);
+		    } catch (Exception e) {
+			System.out.println("Compilation error!");
+			return null;
+		    }
+		}
 
-	if (sts == null) {
-	    System.out.println("No Schemas to process.");
-	    return null;
-	}
-	SchemaType[] globalTypes = sts.globalTypes();
-	SchemaType elem = null;
-	for (SchemaType globalType : globalTypes) {
-	    if (rootName.equals(globalType.getName().getLocalPart())) {
-		elem = globalType;
-		break;
-	    }
-	}
+		if (sts == null) {
+		    System.out.println("No Schemas to process.");
+		    return null;
+		}
+		SchemaType[] globalTypes = sts.globalTypes();
+		SchemaType elem = null;
+		for (SchemaType globalType : globalTypes) {
+			    if (rootName.equals(globalType.getName().getLocalPart())) {
+				elem = globalType;
+				break;
+		    }
+		}
 
-	if (elem == null) {
-	    System.out.println("Could not find a global element with name \"" + rootName + "\"");
-	    return null;
-	}
+		if (elem == null) {
+		    System.out.println("Could not find a global element with name \"" + rootName + "\"");
+		    return null;
+		}
 
-	// Now generate it
-	return SampleXmlUtil.createSampleForType(elem);
+		// Now generate it
+		return SampleXmlUtil.createSampleForType(elem);
     }
 
     /**
@@ -85,21 +85,20 @@ public class XSDs2XMLGenerator {
      * @return список файлов
      */
     private static File[] getAllFilesInDir(String targetDir, final String fileNameFilter) {
-	FilenameFilter filter = new FilenameFilter() {
+		FilenameFilter filter = new FilenameFilter() {
 
 	    @Override
 	    public boolean accept(File dir, String name) {
-		return name.contains(fileNameFilter);
+			return name.contains(fileNameFilter);
 	    }
 	};
-	return new File(targetDir).listFiles(filter);
+		return new File(targetDir).listFiles(filter);
     }
 
     private XSDs2XMLGenerator() {
     }
 
     public static void main(String[] args) {
-	System.out.println(generate(getAllFilesInDir(XSDs2XMLGenerator.class.getResource("../xsd").getFile(), ".xsd"),
-		"FindGibddRequestType"));
+		System.out.println(generate(getAllFilesInDir(XSDs2XMLGenerator.class.getResource("../xsd").getFile(), ".xsd"), "FindGibddRequestType"));
     }
 }
