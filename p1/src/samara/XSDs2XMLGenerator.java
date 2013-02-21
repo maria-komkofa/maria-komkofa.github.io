bcff56bf1cf315c3c1fa596ab437dbeae8c56244
vasiliy.komkov <komkof@gmail.com>
2013-02-21 18:38:33 +0400

    à âîò è êîììèò

--- p1/src/samara/XSDs2XMLGenerator.java
+++ p1/src/samara/XSDs2XMLGenerator.java
@@ -100,5 +100,5 @@ public class XSDs2XMLGenerator {
 
     public static void main(String[] args) {
 		System.out.println(generate(getAllFilesInDir(XSDs2XMLGenerator.class.getResource("../xsd").getFile(), ".xsd"), "FindGibddRequestType"));
-    }
+    }1
 }
